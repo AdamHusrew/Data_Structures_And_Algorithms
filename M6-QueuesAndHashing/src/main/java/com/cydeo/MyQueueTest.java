87a8ff7518e10eb6ecf7664ec02ee03e4fb0bd0e
Adam Husrew <Adam.Husrew@cydeo.com>
2022-12-04 18:36:05 +0300

    Queues And Hashing Done

---
+++ M6-QueuesAndHashing/src/main/java/com/cydeo/MyQueueTest.java
@@ -0,0 +1,16 @@
+package com.cydeo;
+
+public class MyQueueTest {
+    public static void main(String[] args) {
+
+        MyQueue<Integer> mq = new MyQueue<>();
+
+        for (int i = 0; i < 10; i++) {
+            mq.enqueue(i);
+        }
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(mq.dequeue());
+        }
+    }
+}
